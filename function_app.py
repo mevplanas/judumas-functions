9f3c3de8af367c5af8b37b11e0a5a94ffa66e137
osrokas <rokasptr@gmail.com>
2024-07-05 13:51:31 +0300

    create blob trigger function

--- function_app.py
+++ function_app.py
@@ -1,173 +1,26 @@
-# Importing libraries
+# Import necessary libraries
 import azure.functions as func
+from src import azure_api
 import os
-from pathlib import Path
-import yaml
-import logging
-import pandas as pd
-from tqdm import tqdm
-from datetime import datetime
-import time
-import requests
-import ast
 
-# Import custom modules
-from src import azure_api as az
-from src import arcgis
-from src import utils
-
-# Initialazing azure function app
+# Create function app
 app = func.FunctionApp()
 
+# Create blob trigger function
+@app.blob_trigger(arg_name="myblob",path='strava/{name}.zip', connection='AzureWebJobsStorage')
+def my_blob_trigger(blob: func.InputStream):
+        
+    # Create file name from InputStream
+    file_name = blob.name
+    file_name = file_name.split("/")
+    file_name = file_name[1:]
+    blob_name = '/'.join(file_name)
+    
+    # Get connection string and queue name from environment variables
+    conn_str = os.environ["AzureConnectionString"]
+    queue = os.environ["QueueName"]
 
-# Blob trigger decorator
-@app.blob_trigger(
-    arg_name="myblob",
-    path="op-test-adf",
-    connection="judumas_STORAGE",
-    kwargs={},
-)
-def op_mapper(myblob: func.InputStream):
-    """
-    Description
-    --
-    Function detect new zip file in 'op-test' Azure Blob Storage
-    and converts it to multiple csv files. Files than uploading into 'op-test-cs' storage.
-    Also Queque messages are inserted for further processing.
-
-    Parameters
-    --
-    :param myblob : blob input stream
-    """
-
-    # Checks if input file is zip file
-    if myblob.name.endswith(".zip"):
-        name = myblob.name.split("/")[1]
-        name = name.split(".")[0]
-        # Read zip file as bytes
-        data_as_bytes = myblob.read()
-        # Intialazing azure clients
-        data_handler = az.OpTableHandler()
-        blob_handler = az.OpBlobHandler()
-        # Query OsmUpdateLogs table where info with last update date is stored
-        logs_query = "PartitionKey gt ''"
-        max_log = data_handler.query_entities(
-            table_name="OsmUpdateLogs", query=logs_query
-        )
-        # Create dataframe from query results
-        df = pd.DataFrame(data=max_log)
-        # Get max PartitionKey value which is also max date value
-        max_value = df["PartitionKey"].max()
-
-        # Query OsmNetwork table and retrieve Open Street Map network
-        logs_query = f"PartitionKey eq '{max_value}'"
-        data = data_handler.query_entities(table_name="OsmNetwork", query=logs_query)
-        # Create dataframe from retrieved data
-        df = pd.DataFrame(data=data)
-
-        # Leave only neccessary fields
-        df_osm = df[["osm_id", "GlobalID"]]
-
-        # Define needed columns from zip file data
-        columns = ["osm_id", "usage", "filename"]
-
-        final_data = utils.zip_to_list(data_as_bytes, columns)
-        logging.info(len(final_data))
-
-        # Remaping data
-        entities = []
-        for el in final_data:
-            _date = el["filename"].split("/")[1]
-            op_date = _date.split("_")[0]
-            op_hour = _date.split("_")[1]
-            entities.append(
-                {
-                    "osm_id": el["osm_id"],
-                    "usage": el["usage"],
-                    "op_date": op_date,
-                    "op_hour": op_hour,
-                    "filename": el["filename"],
-                    "created_at": utils.get_time()[1],
-                    "updated_at": utils.get_time()[1],
-                    "datetime": f"{op_date[0:4]}-{op_date[4:6]}-{op_date[6:8]} {op_hour}:00:00",
-                }
-            )
-
-        # Create dataframe from remaped data
-        df_op = pd.DataFrame(entities)
-        # Merge OSM network with remaped data
-        data = df_op.merge(df_osm, on="osm_id", how="inner")
-
-        # Convert data to list of dictionaries
-        entities = data.to_dict(orient="records")
-        # Upload data as csv files into Azure Blob Storage
-        # for chunk in tqdm(chunks, total=len(chunks), desc="Inserting data"):
-        blob_handler.upload_blob_csv("op-test-csv-adf", name=op_date, data=entities)
-        # i += 1
-
-    else:
-        logging.info(f"{myblob.name} is not a zip file")
-
-
-@app.route(route="zipuploader", auth_level=func.AuthLevel.FUNCTION)
-def zip_uploader(req: func.HttpRequest) -> func.HttpResponse:
-    """
-    Description
-    --
-    The function creates http request for uplading zip file into blob storage.
-    Path to api endpoint http://{host}/api/zipuploader
-    Also insert date value into JUDUMAS.OP_ROUTES_DATES table
-
-    Parameters
-    --
-    :param req: http request
-    """
-
-    # Initializing current path
-    current_path = Path().resolve()
-
-    # Open confiuration file as a stream
-    with open((os.path.join(current_path, "configuration.yml")), "r") as stream:
-        config = yaml.safe_load(stream=stream)
-
-        # Loading Azure Storage credentials
-    arc_username = config["OPENCITY_PORTAL"]["user"]
-    arc_password = config["OPENCITY_PORTAL"]["password"]
-    arc_portal = config["OPENCITY_PORTAL"]["portal_link"]
-
-    # Initialazing ArcGIS
-    arc_conn = arcgis.ArcGisRestConnector(
-        username=arc_username, password=arc_password, portal=arc_portal
-    )
-
-    # Load file from http request into Azure Blob Storage
-    for input_file in req.files.values():
-        filename = input_file.filename
-        if filename.endswith(".zip"):
-            filename = input_file.filename
-            content = input_file.stream.read()
-            blob_handler = az.OpBlobHandler()
-            blob_handler.upload_blob_zip(
-                content, container="op-test", blob_name=filename
-            )
-            # az_queues = az.OpQueuesHandler('dates-to-insert')
-            # Create datetime object for created_at and updated_at fields
-            now = utils.get_time()[1]
-            datetime_object = (
-                time.mktime(datetime.strptime(now, "%Y-%m-%d %H:%M:%S").timetuple())
-                * 1000
-            )
-            # Create dictionary with data
-            format_date = [
-                {
-                    "date_str": f"{filename[0:4]}/{filename[4:6]}/{filename[6:8]}",
-                    "created_at": int(datetime_object),
-                    "updated_at": int(datetime_object),
-                }
-            ]
-            # Insert data into SQL Server through ArcGIS REST API
-            url = "https://opencity.vplanas.lt/arcgis/rest/services/P_Judumas/Judumas_OP_data_update/FeatureServer/10"
-            values = arc_conn.data_handler(format_date)
-            arc_conn.insert_records(service_url=url, features=values)
-
-    return func.HttpResponse(f"File {str(filename)} uploaded")
+    # Create instance of OpQueuesHandler class
+    queues_handler = azure_api.OpQueuesHandler(queue_name=queue, conn_str=conn_str)
+    # Insert message to queue
+    queues_handler.insert_queque_msg(blob_name)
--- src/azure_api.py
+++ src/azure_api.py
@@ -1,240 +1,8 @@
-# Importing libraries
-from azure.data.tables import TableServiceClient, TableClient
-from azure.storage.blob import BlobServiceClient
 from azure.storage.queue import (
     QueueClient,
     BinaryBase64EncodePolicy,
     BinaryBase64DecodePolicy,
 )
-
-from pathlib import Path
-import os
-import yaml
-import pandas as pd
-import io
-
-# Import custom utils
-from src import utils
-
-
-class OpTableHandler(object):
-    """
-    Initialazing  Azure Table Service client
-    """
-
-    def __init__(self):
-        # Initializing current path
-        current_path = Path().resolve()
-
-        # Open confiuration file as a stream
-        with open((os.path.join(current_path, "configuration.yml")), "r") as stream:
-            config = yaml.safe_load(stream=stream)
-
-        # Loading Azure Storage credentials
-        conn_str = config["AZURE_STORAGE"]["conn_string"]
-        # Connecting to azure table service client via connection string
-        self.service_client = TableServiceClient.from_connection_string(
-            conn_str=conn_str
-        )
-
-    def _table_client(self, table_name: str) -> TableClient:
-        """
-        Description
-        --
-        Connecting to azure table
-
-        Parameters
-        --
-        :param table_name : Azure Storage table name
-
-        Ouput
-        --
-        table_client : Azure Storage Table client
-
-        """
-        table_client = self.service_client.get_table_client(table_name=table_name)
-
-        return table_client
-
-    def query_entities(self, table_name: str, query: str) -> list:
-        """
-        Description
-        --
-        The function query data from Azure Storage table and
-        retrieves OSM street network
-
-        Parameters
-        --
-        :param table_name : Azure Storage Table name
-        :param query : Azure Table Storage queries
-        https://learn.microsoft.com/en-us/rest/api/storageservices/Query-Operators-Supported-for-the-Table-Service?redirectedfrom=MSDN
-
-        Output
-        --
-        entities_list : data queried from Table Storage
-        """
-
-        table_client = self._table_client(table_name)
-        entities = table_client.query_entities(query)
-
-        entities_list = []
-        for entity in entities:
-            entities_list.append(entity)
-
-        return entities_list
-
-    def inset_into_table(self, table_name: str, entities: list) -> None:
-        """
-        Description
-        --
-        The method insert data into Azure Storage Table
-
-        Parameters
-        --
-        :param table_name : Azure Table Storage table name
-        :param entities : list of entities that will be inserted
-        """
-
-        # Connect to Azure Storage Table
-        table_client = self._table_client(table_name)
-        # Create empty list for storing insert operations
-        operations = []
-        # Iterate to each entity
-        for entity in entities:
-            # Create new dictionary for data insertion
-            coverted_entity = {}
-            for key, value in entity.items():
-                coverted_entity[key] = str(value)
-            # Create operation dictionarty for data insertion
-            operations.append(("upsert", coverted_entity))
-
-        # Insert data into Azure Table Storage
-        table_client.submit_transaction(operations)
-
-    def create_entity(self, my_entity: dict) -> dict:
-        """
-        Description
-        --
-        Create single entity for upsert operation.
-
-        Parameters
-        --
-        :param my_entity : dictionary item for data insertion
-
-        Output
-        --
-        converted_entity : entity that will be used in upsert operation
-        """
-
-        date_query = utils.get_time()[0]
-        my_entity.update({"PartitionKey": date_query})
-        my_entity.update({"RowKey": utils.create_random(len_str=8)})
-
-        coverted_entity = {}
-        for key, value in my_entity.items():
-            coverted_entity[key] = str(value)
-
-        return coverted_entity
-
-
-class OpBlobHandler(object):
-    """
-    Initialazing  Azure Blob Service client
-    """
-
-    def __init__(self):
-        current_path = Path().resolve()
-
-        with open((os.path.join(current_path, "configuration.yml")), "r") as stream:
-            config = yaml.safe_load(stream=stream)
-
-        conn_str = config["AZURE_STORAGE"]["conn_string"]
-        self.service_client = BlobServiceClient.from_connection_string(
-            conn_str=conn_str
-        )
-
-    def upload_blob_csv(self, container: str, name: str, data: list) -> None:
-        """
-        Description
-        --
-        The method upload csv file into Azure Blob Storage
-
-        Parameters
-        --
-        :param container : Azure Blob Storage container
-        :param name : filename that will be stored in container
-        :param data : data that will be stored in csv file.
-        """
-
-        # Create dataframe
-        df = pd.DataFrame.from_dict(data)
-
-        # Creating StringIO class for storing data as bytes
-        output = io.StringIO()
-        # Convert dataframe into csv file and store it in memory
-        output = df.to_csv()  #  test it without this
-
-        # Connecting to Azure Blob Service client
-        # and define csv file name for storing
-        blob_client = self.service_client.get_blob_client(
-            container=container, blob=f"{name}.csv"
-        )
-
-        # Append data to output data
-        output = df.to_csv(index=False, encoding="utf-8")
-        # Upload csv files to Azure Blob Storage
-        blob_client.upload_blob(output, blob_type="BlockBlob")
-
-    def read_csv(self, container: str, blob_name: str) -> pd.DataFrame:
-        """
-        Description
-        --
-        The method read csv file and returns pandas DataFrame
-
-        Parameters
-        --
-        :param container : Azure Blob Storage container name
-        :param blob_name : csv blob file name in blob storage
-
-        Output
-        --
-        df : data converted into pandas dataframe
-        """
-
-        # Connect to Azure Blob Service client
-        blob_client = self.service_client.get_blob_client(
-            container=container, blob=blob_name
-        )
-
-        # Reading csv file from blob bytes into dataframe
-        with io.BytesIO() as input_blob:
-            blob_client.download_blob().download_to_stream(input_blob)
-            input_blob.seek(0)
-            df = pd.read_csv(input_blob)
-
-        return df
-
-    def upload_blob_zip(
-        self, input_stream: bytes, container: str, blob_name: str
-    ) -> None:
-        """
-        Description
-        --
-        Upload zip file to Azure Blob Storage
-
-        Parameters
-        --
-        :param input_stream : input file bytes
-        :param container : container name in Azure Blob Storage
-        :param blob_name : blob name in container
-
-        """
-        # Get Azure Blob Storage client
-        blob_client = self.service_client.get_blob_client(
-            container=container, blob=blob_name
-        )
-        # Upload zip files into Azure Blob Storage
-        blob_client.upload_blob(input_stream, blob_type="BlockBlob")
 
 
 class OpQueuesHandler(object):
@@ -242,16 +10,13 @@ class OpQueuesHandler(object):
     Initialazing Azure Queue Service client
     """
 
-    def __init__(self, queue_name: str):
+    def __init__(self, queue_name: str, conn_str: str):
+
         # Defining class parametrs
         self.queue_name = queue_name
+        self.conn_str = conn_str
 
-        current_path = Path().resolve()
-
-        with open((os.path.join(current_path, "configuration.yml")), "r") as stream:
-            config = yaml.safe_load(stream=stream)
-
-        self.conn_str = config["AZURE_STORAGE"]["conn_string"]
+        # Connecting to Azure Queque Service client
         self.service_client = QueueClient.from_connection_string(
             conn_str=self.conn_str,
             queue_name=self.queue_name,
@@ -270,7 +35,7 @@ class OpQueuesHandler(object):
         :param message : queque message
         """
 
-        self.service_client.send_message(message.encode(), visibility_timeout=600)
+        self.service_client.send_message(message.encode(), visibility_timeout=10)
 
     def get_messages(self) -> list:
         """
